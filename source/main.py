b4f2a7fb93020779818795d7d41d6ddc4f21121a
ME <a@b.c>
2021-02-08 23:24:19 +0300

    similar artist and get audio features beta functions

--- source/main.py
+++ source/main.py
@@ -1,51 +1,16 @@
 from Bot.BotHandler import *
-
+from spotify.SpotifyRequest import *
 import asyncio
 from aiohttp import web
 import os
 import json
 
-PORT = int(os.environ.get('PORT', 5000))
-config = ""
-if os.environ.get("HEROKU") is not None:
-    with open("source/Bot/bot_config.json", "r") as conf_file:
-        config = json.load(conf_file)
-        config["webhook_port"] = str(PORT)
-else:
-    with open("Bot/bot_config.json", "r") as conf_file:
-        config = json.load(conf_file)
-        config["webhook_port"] = "8443"
-
-bot = BotHandler(config)
-
-async def check(request):
-    print("MESSAGE GET!")
-    return web.Response()
 
 if __name__ == "__main__":
-    status = ""
-    if os.environ.get("HEROKU") is not None:
-        status = bot.check_webhook()
-        if status == True:
-            print("WEBHOOK OK")
-            config["isWebHookOk"] = 1
-            conf_file = open("source/Bot/bot_config.json", "w")
-            json.dump(config, conf_file)
-            conf_file.close()
-
-
-            app = web.Application()
-            app.router.add_post("/" + config["token"] + "/", check)
-            web.run_app(app, host="0.0.0.0", port=PORT)
-
-
-
-        else:
-            print(status)
-    else:
-        print("WEBHOOK NOT OK:  " + status)
-        while True:
-            updates = bot.getUpdates()
-            if updates is not None:
-                for i in updates:
-                    bot.sendMessage(i["message"]["from"]["id"], "test")
+    with open("spotify/spotify_config.json", "r") as file:
+        conf = json.load(file)
+    client = Spotify(conf)
+    id = input()
+    data = client.similar_artist(id)
+    data = data.json()
+    print(data['tracks'][10]['artists'][0]['external_urls']['spotify'])
--- source/spotify/SpotifyRequest.py
+++ source/spotify/SpotifyRequest.py
@@ -7,12 +7,17 @@ class Spotify:
     def __init__(self, conf):
         self.session = requests.Session()
         self.url = 'https://api.spotify.com/v1/'
-        data = self.session.post(self.url, {'grant_type': 'client_credentials',
+        data = self.session.post('https://accounts.spotify.com/api/token', {'grant_type': 'client_credentials',
                                 'client_id': conf['CLIENT_ID'],
                                 'client_secret': conf['CLIENT_SECRET']}).json()
         self.headers = {
-             'Authorization': 'Bearer {token}'.format(token=data['access_token'])
+            'Authorization': 'Bearer {token}'.format(token=data['access_token']),
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         }
 
     def audio_features(self, id):
-        return self.session.get(self.url + "audio-features/{0}".format(id)).json
+        return self.session.get(self.url + "audio-features/{0}".format(id), headers=self.headers)
+
+    def similar_artist(self, seed_artist):
+        return self.session.get(self.url + "recommendations/?seed_artists={0}".format(seed_artist), headers=self.headers)
